884d0a6eddcb9457aa83460b39f04094e2d9d560
Michael Sverdlik <michael@gigaspaces.com>
2014-05-25 19:29:41 +0300

    add linux vagrant files + provisioning

--- vagrant/linux/provision.sh
+++ vagrant/linux/provision.sh
@@ -2,13 +2,22 @@
 
 # update cache and install essentials
 sudo apt-get update
-sudo apt-get install -y git curl python-dev ruby-dev make
+sudo apt-get install -y git curl python-dev libyaml-dev make
 
 # install pip
 curl --silent --show-error --retry 5 https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | sudo python
 
 # install virtualenv
 sudo pip install virtualenv
+
+# install ruby
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p547.tar.bz2
+tar -xjf ruby-1.9.3-p547.tar.bz2
+cd ruby-1.9.3-p547
+./configure --disable-install-doc
+make
+sudo make install
+cd ~
 
 # install fpm
 sudo gem install fpm --no-rdoc --no-ri
@@ -21,23 +30,17 @@ source ve_pyinstaller/bin/activate
 pip install pyinstaller
 
 # install cfy
-pip install --process-dependency-links https://github.com/cloudify-cosmo/cloudify-cli/archive/develop.zip
-pip install https://github.com/cloudify-cosmo/cloudify-openstack-provider/archive/develop.zip
+pip install --process-dependency-links https://github.com/cloudify-cosmo/cloudify-openstack-provider/archive/develop.zip
 
-# add github fingerprint to known host and clone (must user agent-forwarding)
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-git clone git@github.com:CloudifySource/cloudify-cli-pyinstaller.git
-cd cloudify-cli-pyinstaller
-git checkout develop
+# rm -rf cloudify-cli-packager
+git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
 
 # run pyinstaller
-pyinstaller cfy.spec
+pyinstaller cloudify-cli-packager/pyinstaller/cfy.spec -y
 
 # create deb
 fpm -s dir -t deb -n cfy --prefix /usr/local -C dist/ \
 --version `python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify-cli').version"` \
---after-install after-install.sh --before-remove before-remove.sh \
+--after-install cloudify-cli-packager/packaging/linux/after-install.sh --before-remove cloudify-cli-packager/packaging/linux/before-remove.sh \
 --description "Command line interface for Cloudify" \
 --url "https://github.com/cloudify-cosmo/cloudify-cli" --vendor "GigaSpaces" --license "Apache License 2.0" cfy/
-
-mv cfy_*.deb ~/output
