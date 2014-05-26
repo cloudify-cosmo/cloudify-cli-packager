e928d42a9c476f2a2c77d206d09c3627d5c8a437
Michael Sverdlik <m1keil@users.noreply.github.com>
2014-05-26 18:13:10 +0300

    Update provision.bat

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -1,5 +1,13 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
+
 pip install https://github.com/cloudify-cosmo/cloudify-openstack-provider/archive/develop.zip --process-dependency-links
 pip install pyinstaller
+
+python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify-cli').version" > version.txt
+set /p CFYVERSION=<version.txt
+del version.txt
+
 git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
-pyinstaller cloudify-cli-packager\cfy.spec
+cd cloudify-cli-packager\pyinstaller
+pyinstaller cloudify-cli-packager\pyinstaller\cfy.spec -y
+iscc ..\packaging\windows\inno\cfy_setup.iss
