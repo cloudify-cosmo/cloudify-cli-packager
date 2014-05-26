c0ca0996ee9b13c9553c52a25abcb756d81348b5
Michael Sverdlik <michael@gigaspaces.com>
2014-05-26 16:15:35 +0300

    fix call in provision.bat

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -1,6 +1,4 @@
-cd "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\"
-call vsvars32.bat
-cd %HOME%
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 pip install https://github.com/cloudify-cosmo/cloudify-openstack-provider/archive/develop.zip --process-dependency-links
 pip install pyinstaller
 git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
