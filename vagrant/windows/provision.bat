0e214b4a300f14e769809e5ab41c82757595276c
limor-gs <limor@gigaspaces.com>
2014-06-19 12:47:45 +0300

    Update provision.bat
    move the import pkg_resources after installing cloudify-cli

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -7,10 +7,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.
 
 pip install pyinstaller
 
-python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify-cli').version" > version.txt
-set /p CFYVERSION=<version.txt
-del version.txt
-
 git clone https://github.com/cloudify-cosmo/cloudify-dsl-parser.git
 pushd cloudify-dsl-parser
 	if not (%DSL_SHA%)==() git reset --hard %DSL_SHA%
@@ -35,8 +31,13 @@ pushd cloudify-openstack-provider
 	pip install . -r requirements.txt
 popd
 
+python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify-cli').version" > version.txt
+set /p CFYVERSION=<version.txt
+del version.txt
+
 git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
-pushd cloudify-cli-packager/pyinstaller
+cd cloudify-cli-packager\pyinstaller
 	pyinstaller cfy.spec -y
- 	iscc ../packaging/windows/inno/cfy_setup.iss
-popd
+ 	iscc ..\packaging/windows\inno\cfy_setup.iss
+
+
