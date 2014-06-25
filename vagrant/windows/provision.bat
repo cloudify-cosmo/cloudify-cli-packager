76b2d3311dccc1a2023bbe9d3d3e82864c8738d9
Michael Sverdlik <m1keil@users.noreply.github.com>
2014-06-25 13:12:16 +0300

    Update provision.bat
    add env variable to point to dist folder path

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -34,6 +34,7 @@ popd
 python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify-cli').version" > version.txt
 set /p CFYVERSION=<version.txt
 del version.txt
+set CFYDISTPATH=%HOME%\cloudify-cli-packager\pyinstaller\dist
 
 git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
 cd cloudify-cli-packager\pyinstaller
