a570d8ad02486801774e9d3b39688a6e41a598c6
Idan Moyal <idan@gigaspaces.com>
2015-02-03 17:22:47 +0200

    CFY-2129 added ctx client to windows package

--- pyinstaller/hooks/hook-cloudify.py
+++ pyinstaller/hooks/hook-cloudify.py
@@ -1,2 +1,12 @@
+import os
+
 from PyInstaller.hooks.hookutils import collect_submodules
+from PyInstaller.hooks.hookutils import get_package_paths
+
 hiddenimports = collect_submodules('cloudify')
+
+_relative_ctx_client_path = 'cloudify/proxy'
+_pkg_base, _pkg_dir = get_package_paths('cloudify')
+_full_ctx_client_path = os.path.join(_pkg_base, _relative_ctx_client_path, 'client.py')
+
+datas = [(_full_ctx_client_path, _relative_ctx_client_path)]
