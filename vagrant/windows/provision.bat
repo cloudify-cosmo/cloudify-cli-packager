d5096e09c36b5e13ac0baea83c633b70470068d6
limor-gs <limor@gigaspaces.com>
2015-03-25 15:21:22 +0200

    remove sha ids replace it with pip install from tag

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -1,64 +1,30 @@
-SET DSL_SHA=""
-SET REST_CLIENT_SHA=""
-SET COMMON_PLUGIN_SHA=""
-SET CLI_SHA=""
-SET OS_PROVIDER_SHA=""
-SET OS_PLUGIN_SHA=""
-SET FABRIC_PLUGIN_SHA=""
-SET SCRIPTS_PLUGIN_SHA=""
+REM SET DSL_SHA=""
+REM SET REST_CLIENT_SHA=""
+REM SET COMMON_PLUGIN_SHA=""
+REM SET CLI_SHA=""
+REM SET OS_PROVIDER_SHA=""
+REM SET OS_PLUGIN_SHA=""
+REM SET FABRIC_PLUGIN_SHA=""
+REM SET SCRIPTS_PLUGIN_SHA=""
+
+SET core_tag_name="master"
+SET plugins_tag_name="master"
 
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 
 pip install pyinstaller
 
-git clone https://github.com/cloudify-cosmo/cloudify-dsl-parser.git
-pushd cloudify-dsl-parser
-	if not (%DSL_SHA%)==() git reset --hard %DSL_SHA%
-	pip install .
-popd
+pip install -e git://github.com/cloudify-cosmo/cloudify-dsl-parser.git@%core_tag_name%#egg=cloudify-dsl-parser==%core_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-rest-client.git@%core_tag_name%#egg=cloudify-rest-client==%core_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-plugins-common.git@%core_tag_name%#egg=cloudify-plugins-common==%core_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-script-plugin.git@%plugins_tag_name%#egg=cloudify-script-plugin==%plugins_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-cli.git@%core_tag_name%#egg=cloudify-cli==%core_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-provider.git@%plugins_tag_name%#egg=cloudify-openstack-provider==%plugins_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-plugin.git@%plugins_tag_name%#egg=cloudify-openstack-plugin==%plugins_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-fabric-plugin.git@%plugins_tag_name%#egg=cloudify-fabric-plugin==%plugins_tag_name%
 
-git clone https://github.com/cloudify-cosmo/cloudify-rest-client.git
-pushd cloudify-rest-client
-	if not (%REST_CLIENT_SHA%)==() git reset --hard %REST_CLIENT_SHA%
-	pip install .
-popd
 
-git clone https://github.com/cloudify-cosmo/cloudify-plugins-common.git
-pushd cloudify-plugins-common
-        if not (%COMMON_PLUGIN_SHA%)==() git reset --hard %COMMON_PLUGIN_SHA%
-        pip install .
-popd
-
-git clone https://github.com/cloudify-cosmo/cloudify-script-plugin.git
-pushd cloudify-script-plugin
-        if not (%SCRIPTS_PLUGIN_SHA%)==() git reset --hard %SCRIPTS_PLUGIN_SHA%
-        pip install .
-popd
-
-git clone https://github.com/cloudify-cosmo/cloudify-cli.git
-pushd cloudify-cli
-	if not (%CLI_SHA%)==() git reset --hard %CLI_SHA%
-	pip install .
-popd
-
-git clone https://github.com/cloudify-cosmo/cloudify-openstack-provider.git
-pushd cloudify-openstack-provider
-	if not (%OS_PROVIDER_SHA%)==() git reset --hard %OS_PROVIDER_SHA%
-	pip install .
-popd
-
-git clone https://github.com/cloudify-cosmo/cloudify-openstack-plugin
-pushd cloudify-openstack-plugin
-    if not (%OS_PLUGIN_SHA%)==() git reset --hard %OS_PLUGIN_SHA%
-	pip install .
-popd
-
-git clone https://github.com/cloudify-cosmo/cloudify-fabric-plugin
-pushd cloudify-fabric-plugin
-	if not (%FABRIC_PLUGIN_SHA%)==() git reset --hard %FABRIC_PLUGIN_SHA%
-	pip install .
-popd
 
 python -c "import pkg_resources;print pkg_resources.get_distribution('cloudify').version" > version.txt
 set /p CFYVERSION=<version.txt
