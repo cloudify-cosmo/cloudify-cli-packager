c6bf845c43d87e43a25919495e7139dac6bd20fd
limor-gs <limor@gigaspaces.com>
2015-03-26 12:17:09 +0200

    Update provision.bat

--- vagrant/windows/provision.bat
+++ vagrant/windows/provision.bat
@@ -1,19 +1,18 @@
-SET core_tag_name="master"
-SET plugins_tag_name="master"
-
+SET CORE_TAG_NAME="master"
+SET PLUGINS_TAG_NAME="master"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 
 pip install pyinstaller
 
-pip install -e git://github.com/cloudify-cosmo/cloudify-dsl-parser.git@%core_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-rest-client.git@%core_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-plugins-common.git@%core_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-script-plugin.git@%plugins_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-cli.git@%core_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-provider.git@%plugins_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-plugin.git@%plugins_tag_name%
-pip install -e git://github.com/cloudify-cosmo/cloudify-fabric-plugin.git@%plugins_tag_name%
+pip install -e git://github.com/cloudify-cosmo/cloudify-dsl-parser.git@%CORE_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-rest-client.git@%CORE_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-plugins-common.git@%CORE_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-script-plugin.git@%PLUGINS_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-cli.git@%CORE_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-provider.git@%PLUGINS_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-openstack-plugin.git@%PLUGINS_TAG_NAME%
+pip install -e git://github.com/cloudify-cosmo/cloudify-fabric-plugin.git@%PLUGINS_TAG_NAME%
 
 
 
@@ -23,7 +22,7 @@ del version.txt
 
 git clone https://github.com/cloudify-cosmo/cloudify-cli-packager.git
 cd cloudify-cli-packager\pyinstaller
-  git checkout -b tmp_branch %core_tag_name%
+  git checkout -b tmp_branch %CORE_TAG_NAME%
 	git log -1
   pyinstaller cfy.spec -y
   iscc ..\packaging/windows\inno\cfy_setup.iss
